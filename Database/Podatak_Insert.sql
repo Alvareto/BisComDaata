1d7aea46525b878b513434548ab5c7d028f23a57
Ivan Grgurina <ivan.grgurina@gmail.com>
2014-12-01 00:08:23 +0100

    Initial application

---
+++ Database/Podatak_Insert.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[Podatak_Insert]
+	@ime nvarchar(40),
+	@prezime nvarchar(60),
+	@pbr int,
+	@grad nvarchar(60),
+	@telefon nvarchar(15)
+AS
+BEGIN
+	INSERT INTO [dbo].[Podatak] ([Ime], [Prezime], [PostanskiBroj], [Grad], [Telefon])
+	VALUES (@ime, @prezime, @pbr, @grad, @telefon)
+	
+	SELECT SCOPE_IDENTITY() AS Id
+END
