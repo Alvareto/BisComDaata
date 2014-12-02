a35d6dc2ee6dac290303454069b73b90867497db
Ivan Grgurina <ivan.grgurina@gmail.com>
2014-12-02 16:41:29 +0100

    create podatak view model #4

--- Web/Models/PodatakViewModel.cs
+++ Web/Models/PodatakViewModel.cs
@@ -5,8 +5,29 @@ using System.Web;
 
 namespace Web.Models
 {
+    /// <summary>
+    /// Same as Podatak model, but holds PostalCode as string instead of integer
+    /// for easier interaction with view.
+    /// </summary>
     public class PodatakViewModel
     {
+        // used when saving data to database to skid entries with invalid postal code
+        // TODO: can this be done better?
+        public const string INVALID_POSTAL_CODE = "INVALID";
 
+        //private Podatak _podatak; -- we don't need it
+        public String Ime { get; set; }
+        public String Prezime { get; set; }
+        public String PostanskiBroj { get; set; }
+        public String Grad { get; set; }
+        public String Telefon { get; set; }
+
+        public PodatakViewModel(String pBr)
+        {
+            // pokušaj parsirati poštanski broj kao integer
+            // ukoliko ne uspije, proglasi ga nevaljanim
+            int _pom;
+            PostanskiBroj = Int32.TryParse(pBr, out _pom) ? pBr : INVALID_POSTAL_CODE;
+        }
     }
 }
