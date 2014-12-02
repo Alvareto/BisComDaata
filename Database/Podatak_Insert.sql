6a968b89a5111fee5842db0fd14fb72d3e985b66
Ivan Grgurina <ivan.grgurina@gmail.com>
2014-12-02 10:48:44 +0100

    better formatting #2

--- Database/Podatak_Insert.sql
+++ Database/Podatak_Insert.sql
@@ -1,13 +1,29 @@
 ﻿CREATE PROCEDURE [dbo].[Podatak_Insert]
-	@ime nvarchar(40),
-	@prezime nvarchar(60),
-	@pbr int,
-	@grad nvarchar(60),
-	@telefon nvarchar(15)
+	@ime 		nvarchar(40),
+	@prezime 	nvarchar(60),
+	@pbr 		int,
+	@grad 		nvarchar(60),
+	@telefon 	nvarchar(15)
 AS
 BEGIN
-	INSERT INTO [dbo].[Podatak] ([Ime], [Prezime], [PostanskiBroj], [Grad], [Telefon])
-	VALUES (@ime, @prezime, @pbr, @grad, @telefon)
+	SET NOCOUNT ON -- maybe
+
+	INSERT INTO [dbo].[Podatak]
+	(
+		[Ime],
+		[Prezime], 
+		[PostanskiBroj], 
+		[Grad], 
+		[Telefon]
+	)
+	VALUES 
+	(
+		@ime,
+		@prezime, 
+		@pbr, 
+		@grad, 
+		@telefon
+	)
 	
 	SELECT SCOPE_IDENTITY() AS Id
 END
