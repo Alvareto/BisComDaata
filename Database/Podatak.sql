3669f300809f804b9750633ddba6dfd2cc860c55
Ivan Grgurina <ivan.grgurina@gmail.com>
2014-12-03 19:44:38 +0100

    Update Podatak.sql

--- Database/Podatak.sql
+++ Database/Podatak.sql
@@ -1,14 +1,10 @@
--- Due to naming conventions, I renamed table from Podaci to Podatak.
-CREATE TABLE [dbo].[Podatak]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
-	[Ime] NVARCHAR(40) NOT NULL,
-	[Prezime] NVARCHAR(60) NOT NULL,
-	[PostanskiBroj] INTEGER NOT NULL,
-	[Grad] NVARCHAR(60) NOT NULL,
-	[Telefon] NVARCHAR(15) NOT NULL
+CREATE TABLE [dbo].[Podatak] (
+    [Id]            INT           IDENTITY (1, 1) NOT NULL,
+    [Ime]           NVARCHAR (40) NOT NULL,
+    [Prezime]       NVARCHAR (60) NOT NULL,
+    [PostanskiBroj] INT           NOT NULL,
+    [Grad]          NVARCHAR (60) NOT NULL,
+    [Telefon]       NVARCHAR (15) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT uc_person UNIQUE(Ime, Prezime, Telefon)
 );
-GO
-ALTER TABLE [dbo].[Podatak]
-  ADD CONSTRAINT ucOsoba UNIQUE (Ime, Prezime, Telefon);
-GO
